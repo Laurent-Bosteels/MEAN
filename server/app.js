ec72596d1dcd04cf1026342f16e7deeb801aa227
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-08-09 19:13:34 +0200

    allFriends array

--- server/app.js
+++ server/app.js
@@ -1,59 +1,53 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
 const app = express();
-const Friend = require('./models/friend.model')
+const Friend = require("./models/friend.model");
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}) );
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.all("/*", function(req, res, next){
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    next();
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+  next();
 });
 
 /* Mongoose connection */
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Laurent:Laurent123@friendbook.kqznb.mongodb.net', {useNewUrlParser: true, useUnifiedTopology: true});
+const mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://Laurent:Laurent123@friendbook.kqznb.mongodb.net/test",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 const db = mongoose.connection;
-db.once('open', () => {
-console.log('connected');
-})
+db.once("open", () => {
+  console.log("connected");
+});
 
 /* Route handlers */
 /* List Routes */
+let allFriends = [];
 
-/* GET  */
-app.get('/',function (request, response){
-    console.log('Hello World');
-    let friend = new Friend({
-        fname: 'Eddy',
-        lname: 'Merckx',
-        phone: '0469696969',
-        email: 'eddymerckx@becode.com',
-        language: 'PHP',
-    });
-
-    friend.save()
-
+app.get('/', function (request, response) {
+    response.send('Hello from server');
 });
 
-
-/* GET /friends
-Purpose: Get all friends */
 app.get('/allFriends',function (request, response){
     response.send(allFriends);
 });
 
-/* POST /friends
-Purpose: Create a new friend */
+app.post('/', function (request, response) {
+    response.status(200).send({"message": "Data received"});
+});
+
 app.post('/allFriends', function (request, response) {
     allFriends.push(request.body);
     response.status(200).send({"message" : "Data Received"});
 });
 
 app.listen(3000, () => {
-console.log("Server is listening on port 3000");
-})
+  console.log("Server is listening on port 3000");
+});
